884e911fb75f3a625994cba1417c07fe6e2e869f
Araya Alejo <103607905+Araya-Alejo@users.noreply.github.com>
2022-11-16 11:52:38 -0300

    .

---
+++ capaEntidad/Seats.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using capaEntidad;
+
+namespace capaEntidad
+{
+    internal class Seats
+    {
+
+        private long id;
+        private DateTime date;
+        private List<Movement> fk_movement;
+
+        public long Id { get => id; set => id = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public List<Movement> Fk_movement { get => fk_movement; set => fk_movement = value; }
+        public Seats(int id, DateTime date, List<Movement> fk_movement)
+        {
+            this.id = id;
+            this.date = date;
+            this.fk_movement = fk_movement;
+        }
+
+        public Seats()
+        {
+        }
+    }
+}
