66942caa72582fd5437c4d679fedbc057b324aaf
Asiel <asiel.alvarez@alumnos.frm.utn.edu.ar>
2022-11-16 17:33:50 -0300

    Persist & Read with Serialize

--- capaDatos/Base.cs
+++ capaDatos/Base.cs
@@ -1,7 +1,9 @@
-﻿using MySql.Data.MySqlClient;
+﻿using capaEntidad;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,9 @@ namespace capaDatos
 {
     public class Base
     {
+        private FileStream file;
+        private BinaryFormatter formatter;
+
         public string CadenaConexion = "Server=localhost;User=root;Password=;Port=3306;database=contabilidad_db";
 
         public MySqlConnection Conectar()
@@ -21,6 +26,39 @@ namespace capaDatos
         public void Desconectar(MySqlConnection mySqlConnection)
         {
             mySqlConnection.Close();
+        }
+
+        public bool PersistBlockchain(Blockchain block)
+        {
+            try
+            {
+                file = new FileStream("file.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                formatter = new BinaryFormatter();
+                formatter.Serialize(file, block);
+                return true;
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public Blockchain ReadBlockchain()
+        {
+            Blockchain chain = null;
+
+            try
+            {
+                file = new FileStream("file.txt", FileMode.Open, FileAccess.Read);
+                formatter = new BinaryFormatter();
+                chain = (Blockchain)formatter.Deserialize(file);
+                file.Close();
+                return chain;
+            }catch(Exception e)
+            {
+                Blockchain newBc = new Blockchain();
+                return newBc;
+            }
         }
     }
 }
