47a28869543f0289cb74400867ab563b88460ee9
Araya Alejo <103607905+Araya-Alejo@users.noreply.github.com>
2022-11-17 13:39:56 -0300

    Update Account.cs

--- capaEntidad/Account.cs
+++ capaEntidad/Account.cs
@@ -10,14 +10,11 @@ namespace capaDatos
         private String nombre;
         private float debe;
         private float haber;
-        private int tipo;
-        public string Hash { get { return HashHelper.CalculateHash(string.Format("{0}{1}{2}{3}{4}", nombre, tipo, debe, haber, fecha)); } }
 
         public String _fecha { get => _fecha; set { _fecha = value; } }
         public String _nombre { get => _nombre; set { _nombre = value; } }
         public float _debe { get => _debe; set { _debe = value; } }
         public float _haber { get => _haber; set { _haber = value; } }
-        public int _tipo { get => _tipo; set { _tipo = value; } }
 
         public Account(string nombre)
         {
@@ -28,7 +25,6 @@ namespace capaDatos
         {
             this.fecha = fecha;
             this.nombre = nombre;
-            this.tipo = tipo;
 
             if (debeOHaber == 0)
             {
