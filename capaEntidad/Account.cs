6c453dccc7c1b4d758ef04cf78955cbb3245ff85
Asiel <asiel.alvarez@alumnos.frm.utn.edu.ar>
2022-11-17 14:00:10 -0300

    .

--- capaEntidad/Account.cs
+++ capaEntidad/Account.cs
@@ -20,6 +20,30 @@ namespace capaDatos
         {
             this._Nombre = nombre;
         }
+        public String _fecha { get => _fecha; set { _fecha = value; } }
+        public String _nombre { get => _nombre; set { _nombre = value; } }
+        public float _debe { get => _debe; set { _debe = value; } }
+        public float _haber { get => _haber; set { _haber = value; } }
+
+        public Account(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public Account(string nombre, int tipo, float monto, int debeOHaber, string fecha)
+        {
+            this.fecha = fecha;
+            this.nombre = nombre;
+
+            if (debeOHaber == 0)
+            {
+                this.debe = monto;
+            }
+            else
+            {
+                this.haber = monto;
+            }
+        }
 
     }
 }
