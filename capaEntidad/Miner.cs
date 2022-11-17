d48afb76ba15a81efb26ccccf7bf87165ff1e9cc
Araya Alejo <103607905+Araya-Alejo@users.noreply.github.com>
2022-11-17 13:31:00 -0300

    Create Miner.cs

---
+++ capaEntidad/Miner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaEntidad
+{
+    /// <summary>
+    /// filter of seat and block
+    /// </summary>
+    internal class Miner
+    {
+
+
+
+
+
+    }
+}
