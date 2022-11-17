7daa83affd053b9cf9270b5224de602347fe7a6b
Asiel <asiel.alvarez@alumnos.frm.utn.edu.ar>
2022-11-17 14:33:53 -0300

    .

--- capaDatos/Base.cs
+++ capaDatos/Base.cs
@@ -31,24 +31,6 @@ namespace capaDatos
             }
         }
 
-        public static bool PersistAccount(List<Account> account)
-        {
-            try
-            {
-                file = new FileStream("block.txt", FileMode.OpenOrCreate, FileAccess.Write);
-                file = new FileStream("account.txt", FileMode.OpenOrCreate, FileAccess.Write);
-                formatter = new BinaryFormatter();
-                formatter.Serialize(file, account);
-                file.Close();
-                return true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return false;
-            }
-        }
-
         public static Blockchain ReadBlockchain()
         {
             Blockchain chain = null;
@@ -70,81 +52,32 @@ namespace capaDatos
         public List<string> carga_combo()
         {
             return new List<string>
-        {
-            "CAJA",
-            "BANCO XXX C/C",
-            "BANCO XXX C/A",
-            "VALORES A DEPOSITAR",
-            "VALORES DIFERIDOS A DEPOSITAR",
-            "CHEQUES RECHAZADOS",
-            "MONEDA EXTRANJERA",
-            "TARJETA DE CREDITO XXX",
-            "MERCADERIAS",
-            "MATERIAS PRIMAS",
-            "MUEBLES Y ÚTILES",
-            "INSTALACIONES",
-            "INMUEBLES",
-            "RODADOS",
-            "MAQUINARIAS",
-            "EQUIPOS DE COMPUTACIÓN",
-            "DEUDORES POR VENTAS",
-            "DEUDORES VARIOS",
-            "DEUDORES MOROSOS",
-            "DEUDORES EN GESTIÓN JUDICIAL",
-            "PREVISION DEUDORES INCOBRALES",
-            "PREVISION POR DESVALORIZACIÓN DE BIENES DE CAMBIO",
-            "DOCUMENTOS A COBRAR",
-            "IVA A FAVOR"
-        };
-        }
-
-        public void Carga_de_datos()
-        {
-            //Blockchain blockchain = new Blockchain();
-            //Account caja = new Account("Caja", 0, 200, 0, "20/11/2022");
-            //Account banco = new Account("Banco galicia", 0, 300, 0, "21/11/2022");
-            //List<Account> listaCuentas = new List<Account>();
-            //listaCuentas.Add(caja);
-            //blockchain._TempAccount.Add(caja);
-            //blockchain._TempAccount.Add(banco);
-
-
-            List<Account> listaCuentas = new List<Account>();
-            listaCuentas.Add(new Account("CAJA"));
-            listaCuentas.Add(new Account("BANCO XXX C/C"));
-            listaCuentas.Add(new Account("BANCO XXX C/A"));
-            listaCuentas.Add(new Account("VALORES A DEPOSITAR"));
-            listaCuentas.Add(new Account("VALORES DIFERIDOS A DEPOSITAR"));
-            listaCuentas.Add(new Account("CHEQUES RECHAZADOS"));
-            listaCuentas.Add(new Account("MONEDA EXTRANJERA"));
-            listaCuentas.Add(new Account("TARJETA DE CREDITO XXX"));
-            listaCuentas.Add(new Account("MERCADERIAS"));
-            listaCuentas.Add(new Account("MATERIAS PRIMAS"));
-            listaCuentas.Add(new Account("MUEBLES Y ÚTILES"));
-            listaCuentas.Add(new Account("INSTALACIONES"));
-            listaCuentas.Add(new Account("INMUEBLES"));
-            listaCuentas.Add(new Account("RODADOS"));
-            listaCuentas.Add(new Account("MAQUINARIAS"));
-            listaCuentas.Add(new Account("EQUIPOS DE COMPUTACIÓN"));
-            listaCuentas.Add(new Account("DEUDORES POR VENTAS"));
-            listaCuentas.Add(new Account("DEUDORES VARIOS"));
-            listaCuentas.Add(new Account("DEUDORES MOROSOS"));
-            listaCuentas.Add(new Account("DEUDORES EN GESTIÓN JUDICIAL"));
-            listaCuentas.Add(new Account("PREVISION DEUDORES INCOBRALES"));
-            listaCuentas.Add(new Account("PREVISION POR DESVALORIZACIÓN DE BIENES DE CAMBIO"));
-            listaCuentas.Add(new Account("DOCUMENTOS A COBRAR"));
-            listaCuentas.Add(new Account("IVA A FAVOR"));
-
-
-            if (Base.PersistAccount(listaCuentas))
             {
-                MessageBox.Show("datos guardados");
-            }
-            else
-            {
-                MessageBox.Show("datos no guardados");
-            }
-            
+                "CAJA",
+                "BANCO XXX C/C",
+                "BANCO XXX C/A",
+                "VALORES A DEPOSITAR",
+                "VALORES DIFERIDOS A DEPOSITAR",
+                "CHEQUES RECHAZADOS",
+                "MONEDA EXTRANJERA",
+                "TARJETA DE CREDITO XXX",
+                "MERCADERIAS",
+                "MATERIAS PRIMAS",
+                "MUEBLES Y ÚTILES",
+                "INSTALACIONES",
+                "INMUEBLES",
+                "RODADOS",
+                "MAQUINARIAS",
+                "EQUIPOS DE COMPUTACIÓN",
+                "DEUDORES POR VENTAS",
+                "DEUDORES VARIOS",
+                "DEUDORES MOROSOS",
+                "DEUDORES EN GESTIÓN JUDICIAL",
+                "PREVISION DEUDORES INCOBRALES",
+                "PREVISION POR DESVALORIZACIÓN DE BIENES DE CAMBIO",
+                "DOCUMENTOS A COBRAR",
+                "IVA A FAVOR"
+            };
         }
     }
 }
